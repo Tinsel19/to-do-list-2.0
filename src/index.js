b1a576a376478416cc9369d7e69d43855cdb278d
Tinsel19 <107304041+Tinsel19@users.noreply.github.com>
2023-07-07 14:30:10 +0100

    src root

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import Calc from '../src/calculator/calc';
+import '../src/styles.css';
+import App from './web1/App';
+// import App from '../src/card/App';
+// import App from './form/App';
+// import App from './count/App';
+// import App from './class/App';
+
+ReactDOM.render(
+        <div>
+            <App/>
+        </div>
+        
+        
+        , document.getElementById("root"));
